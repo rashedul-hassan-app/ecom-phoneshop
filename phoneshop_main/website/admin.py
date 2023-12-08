d8e2d480818e54bb756943a4532f5a42a8a1edc1
Rashedul Hassan <rashedul@outlook.com>
2023-12-08 17:34:45 +0000

    Set up 4 models and uploaded product details via Admin portal

--- phoneshop_main/website/admin.py
+++ phoneshop_main/website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer, Order, Product, Category
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(Category)
