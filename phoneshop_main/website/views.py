0d6b1763307db7e1c0af6b8d901a3510c325f603
Rashedul Hassan <rashedul@outlook.com>
2023-12-11 18:33:23 +0000

    Login functionality

--- phoneshop_main/website/views.py
+++ phoneshop_main/website/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Product
 # Create your views here.
 
@@ -6,3 +8,21 @@ from .models import Product
 def index(request):
     all_products = Product.objects.all()
     return render(request, 'index.html', {'products': all_products})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You are now Signed in!"))
+            return redirect('index')
+        else:
+            messages.error(
+                request, ("Unable to sign in. Please check your details and try again"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
