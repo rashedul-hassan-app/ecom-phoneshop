1efe4a58e686fcb6370e2ae6ed4a7d1c446b9a1c
Rashedul Hassan <rashedul@outlook.com>
2023-12-11 17:29:43 +0000

    Added Sale feature

--- phoneshop_main/website/models.py
+++ phoneshop_main/website/models.py
@@ -32,6 +32,10 @@ class Product(models.Model):
         max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
+    # Add sale option
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=7)
+
     def __str__(self):
         return self.name
 
