71d53057767918febd4c05f11689c9280c469e92
Rashedul Hassan <rashedul@outlook.com>
2023-12-12 17:01:38 +0000

    Logout added

--- phoneshop_main/website/views.py
+++ phoneshop_main/website/views.py
@@ -26,3 +26,9 @@ def login_user(request):
             return redirect('login')
     else:
         return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.error(request, ("You have been logged out!"))
+    return redirect('index')
