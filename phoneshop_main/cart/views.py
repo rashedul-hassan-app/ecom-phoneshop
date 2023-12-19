4e3f370ec6a0081a8699dd0242c254418af39272
Rashedul Hassan <rashedul@outlook.com>
2023-12-19 18:04:41 +0000

    Add to cart getting 200, full functionality not implemented yet

---
+++ phoneshop_main/cart/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from website.models import Product
+from django.http import JsonResponse
+# Create your views here.
+
+
+def cart_summary(request):
+    return render(request, 'cart_summary.html', {})
+
+
+def cart_add(request):
+    print("--- inside the cart_add function ---")
+    #  Get the cart object
+    cart = Cart(request)
+
+    # test for POST
+    if request.POST.get('action') == 'post':
+        # Get our product
+        product_id = int(request.POST.get('product_id'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        # save to session
+        cart.add(product=product)
+
+        # return the response
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
+
+
+def cart_update(request):
+    pass
+
+
+def cart_delete(request):
+    pass
--- phoneshop_main/phoneshop_main/urls.py
+++ phoneshop_main/phoneshop_main/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'), name='website'),
+    path('cart/', include('cart.urls'), name='cart'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
